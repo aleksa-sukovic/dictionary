be06affbfac328d0a01a8d3a3c590083da9c6200
Aleksa Sukovic <sukovic.aleksa@gmail.com>
2019-12-27 17:56:42 +0100

    Adds Bootstrap

--- src/index.php
+++ src/index.php
@@ -1,12 +1,13 @@
-<?php
-
-require_once './autoload.php';
-
-?>
+<?php require_once './autoload.php'; ?>
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+
+    <!-- Bootstrap -->
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
     <title>Dictionary</title>
 </head>
 <body>
